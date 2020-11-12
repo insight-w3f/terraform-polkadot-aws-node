769df1c84677034bbce2dbfdbc56693b504fdedc
Richard Mah <dick.mah@gmail.com>
2020-11-12 15:34:45 -0500

    fix default test for upstream network changes

--- examples/defaults/main.tf
+++ examples/defaults/main.tf
@@ -3,16 +3,16 @@ variable "private_key_path" {}
 variable "create_ansible" {}
 
 module "network" {
-  source         = "github.com/insight-infrastructure/terraform-aws-polkadot-network.git?ref=master"
-  sentry_enabled = true
-  num_azs        = 1
+  source      = "github.com/insight-infrastructure/terraform-aws-polkadot-network.git?ref=master"
+  api_enabled = true
+  num_azs     = 1
 }
 
 module "default" {
   source            = "../.."
   public_key        = var.public_key
   subnet_id         = module.network.public_subnets[0]
-  security_group_id = module.network.sentry_security_group_id
+  security_group_id = module.network.api_security_group_id
   private_key_path  = var.private_key_path
   create_ansible    = var.create_ansible
   node_purpose      = "library"
